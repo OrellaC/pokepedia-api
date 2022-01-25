62b70108fcf1aac1ef037ebc015c061b14ba6973
orellachichester@gmail.com <orellachichester@gmail.com>
2022-01-25 14:47:49 -0500

    Updated bootstrap and login form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useContext } from "react";
+import { Routes, Route } from 'react-router-dom'
 //Components 
 import Nav from "./components/Nav"
+//Pages
+import Login from "./pages/Login";
 //Contexts
 import UserContext from "./contexts/UserContext";
 //Css
@@ -16,6 +19,16 @@ function App() {
       {/* All context comes with the Provider Component. This allows us to use this as a wrapper and share information to all of its children. We need the value prop inside inside our provider. */}
       <UserContext.Provider value={'Christina'}>
         <Nav />
+
+        {/* We need to wrap all our routes inside react router Routes component  */}
+        <Routes>
+          <Route path='login' element ={<Login />} />
+          {/* <Route path='/' element ={<Pokepedia />} />
+          <Route path='pokemon list' element={<PokemonList />} /> */}
+
+        </Routes>
+
+
       </UserContext.Provider>
 
 
